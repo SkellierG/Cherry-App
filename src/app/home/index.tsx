9df56b21ec7525b9578c1acbb076a2173145187a
SkellierG <benja314gonzalez@gmail.com>
2025-01-03 21:44:08 -0300

    backgrownd color test

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -1,10 +1,23 @@
 import React from "react";
 import DummyScreen from "@screens/dummy";
 import { View } from "react-native";
+import { useDynamicStyles } from "@hooks/useDynamicStyles";
+import {
+	dark_default_theme,
+	light_default_theme,
+} from "@assets/themes/tamagui-rnp-adapter";
 
-export default function ProfileScreen() {
+export default function HomeScreen() {
+	const styles = useDynamicStyles((theme) => ({
+		view: {
+			backgroundColor:
+				theme === "dark"
+					? dark_default_theme.colors.background
+					: light_default_theme.colors.background,
+		},
+	}));
 	return (
-		<View>
+		<View style={styles.view}>
 			<DummyScreen screenName="Home" />
 		</View>
 	);
